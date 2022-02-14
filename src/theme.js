657f1426f9cfe762546f4b2aac59913879338a83
BoDee <nyx0hemir@gmail.com>
2022-02-14 06:09:17 -0700

    adding MUI theme

---
+++ src/theme.js
@@ -0,0 +1,22 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+    palette: {
+        type: 'light',
+        rain: {
+            main: '#95ADB6'
+        },
+        rose: {
+            main: '#CBB3BF'
+        },
+        malachite: {
+            main: '#374B4A'
+        },
+        smoke: {
+            main: '#1D1E2C'
+        },
+        overcast: {
+            main: '#8DA1B9'
+        }
+    }
+});
