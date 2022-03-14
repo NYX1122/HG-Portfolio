312a8905441331d4802fb94536ae3df51e6b6ec5
BoDee Angus <bodee.angus@gmail.com>
2022-03-14 16:08:28 -0600

    added loop that iterates through artwork

---
+++ src/components/PieceItem/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+export default function Header({ imgName }) {
+    console.log(imgName);
+
+    return (
+        <Box sx={{
+            width: 342,
+            height: 190.5,
+            background: 'linear-gradient(to bottom right, #FBFE5C, #FC6BFF, #FFFFFF)',
+            borderRadius: '25px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginBottom: '10px'
+        }}>
+            <Box sx={{
+                backgroundImage: `url(/art/${imgName}.jpg)`,
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+                width: 338,
+                height: 186.5,
+                borderRadius: '24px'
+            }}>
+            </Box>
+        </Box>
+    )
+}
--- src/pages/Pieces.jsx
+++ src/pages/Pieces.jsx
@@ -1,7 +1,38 @@
+import PieceItem from '../components/PieceItem';
+
 export default function Pieces() {
-    return (
-      <main>
-        <h2>Pieces Page</h2>
-      </main>
-    );
+  const artList = [
+    {
+      id: 1,
+      name: 'little_girl_steps',
+      description: 'A little girl sitting on stairs surrounded by flowers.',
+    },
+    {
+      id: 2,
+      name: 'marylin_monroe_portrait',
+      description: 'Marylin Monroe smiling.'
+    },
+    {
+      id: 3,
+      name: 'full_moon_night',
+      description: 'A full moon at night.'
+    },
+    {
+      id: 4,
+      name: 'brown_bear_dog',
+      description: 'A brown dog named Bear.'
+    }
+  ]
+
+  return (
+    <main>
+      {artList.map((image) => (
+        <PieceItem
+          imgName={image.name}
+          alt={image.name}
+          key={image.id}
+        ></PieceItem>
+      ))}
+    </main>
+  );
 }
